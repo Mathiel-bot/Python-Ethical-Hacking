d7c230c01481625637e2e5997ee4254c60003763
Oti Mathiel <mathiel@vbox.Mathiei>
2025-04-03 07:17:44 -0400

    This is webdeves class code

---
+++ class-demo/hello.py
@@ -0,0 +1,25 @@
+# This line is running python code without a variable
+print("Hello World!")
+print('I am learning python')
+
+
+# Declaring a variable
+First_name = "Oti"
+Last_name = "Mathiel"
+
+# Concatenation (concatenating the variables)
+print(First_name + " " + Last_name)
+
+#Types of variables
+# Integer- whole number (eg: 0-9)
+# floating-point (float) - Decimal numbers (eg: 0.1)
+# string(str) - Text Data ("hello", "Python" 'Text')
+# Boolean(bool) - True/false values
+
+age = 18
+sentence = "The legal age to vote in"
+country = "United Kingdom"
+
+full_setence = sentence + " " + country
+
+print(full_setence + " is " + str(age))
